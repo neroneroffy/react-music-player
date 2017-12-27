6bd2760a9c955405a6ed47b532b88c83f74f0ec4
capslocktao <haitaocapslock@outlook.com>
2017-12-27 15:47:57 +0800

    播放进度条

--- src/react-music-player/react-music-player.js
+++ src/react-music-player/react-music-player.js
@@ -5,52 +5,103 @@ class ReactMusicPlayer extends Component {
         super(props);
         this.state = {
             isPaused:false,
-            totalTime:0
+            totalTime:0,
+            playPer:0,
+            bufferedPer:0,
+            playedLeft:0,
+            remainTime:0
         };
         this.last = this.last.bind(this);
         this.play = this.play.bind(this);
         this.next = this.next.bind(this);
+        this.startChangeTime = this.startChangeTime.bind(this)
+        this.moveProgress = this.moveProgress.bind(this)
     }
     componentDidMount(){
-        this.refs.audio.addEventListener('canplaythrough',()=>{
-            let musicTime = parseInt(this.refs.audio.duration);
-            if(musicTime<60){
-                musicTime = `00:${musicTime<10?`0${musicTime}`:musicTime}`
-            }else{
-                musicTime = `${parseInt(musicTime/60)<10?`0${parseInt(musicTime/60)}`:parseInt(musicTime/60)}:${musicTime%60<10?`0${musicTime%60}`:musicTime%60}`
-            }
-            this.setState({
-                totalTime:musicTime
-            })
+        let audio = this.refs.audio
+        audio.addEventListener('canplay',()=>{
+            //获取总时间
+            let totalTime = parseInt(this.refs.audio.duration);
 
+            this.setState({
+                totalTime:this.getTime(totalTime),
+                remainTime:this.getTime(totalTime),
+                playedLeft:this.refs.played.getBoundingClientRect().left
+            });
+            //
         })
+
     }
     last(){}
     play(){
-        totalTime:console.log(this.refs.audio);
-        if(this.refs.audio.paused){
-            this.refs.audio.play()
+        let audio = this.refs.audio;
+        if(audio.paused){
+            audio.play()
             this.setState({
-                isPaused:true
+                isPaused:true,
+
             })
         }else{
-            this.refs.audio.pause()
+            audio.pause()
             this.setState({
                 isPaused:false
             })
-
         }
+        audio.addEventListener('timeupdate',()=>{
+            //设置播放进度条
+            let playPer = audio.currentTime/audio.duration;
+            this.refs.played.style.width = playPer*100+"%";
+            //设置缓冲进度条
+            let timeRages = audio.buffered;
+            let bufferedTime = timeRages.end(timeRages.length-1);
+            let bufferedPer = bufferedTime/audio.duration;
+            this.refs.buffered.style.width = bufferedPer*100+"%";
+            //设置剩余时间
+            let remainTime = parseInt(audio.duration - audio.currentTime);
+            this.setState({
+                remainTime:this.getTime(remainTime)
+            },()=>{
+                console.log(this.state.remainTime)
+            })
+            if(playPer>bufferedPer){
+
+            }
+        })
 
 
     }
     next(){}
+    startChangeTime(e){
+        this.setTime(e)
+    }
+    moveProgress(e){
+        let audio = this.refs.audio;
+        if(audio.currentTime !== 0){
+            this.setTime(e)
+        }
+    }
+    getTime(musicTime){
+        if(musicTime<60){
+            musicTime = `00:${musicTime<10?`0${musicTime}`:musicTime}`
+        }else{
+            musicTime = `${parseInt(musicTime/60)<10?`0${parseInt(musicTime/60)}`:parseInt(musicTime/60)}:${musicTime%60<10?`0${musicTime%60}`:musicTime%60}`
+        }
+        return musicTime
+    }
+    setTime(e){
+
+        let audio = this.refs.audio;
+        let newWidth = (e.touches[0].pageX-this.state.playedLeft)/this.refs.progress.offsetWidth
+        this.refs.played.style.width = newWidth*100 + "%";
+        audio.currentTime = newWidth*audio.duration
+
+    }
     render() {
         return (
             <div id="react-music-player">
                 <div className="react-music-player-inner" >
                     <div className="left-control">
                         <span className="icon-last" onClick={this.last}></span>
-
                         <span className={this.state.isPaused?"icon-333":"icon-44"} onClick={this.play}></span>
                         <span className="icon-next" onClick={this.next}></span>
                     </div>
@@ -62,10 +113,20 @@ class ReactMusicPlayer extends Component {
                             <div className="music-name">
                                 西城男孩：My Love
                             </div>
-                            <div className="progress"></div>
+                            <div className="progress-wrapper" ref="progress"
+                                 onTouchMove={this.moveProgress}
+                                 onTouchStart={this.startChangeTime}
+                            >
+                                <div className="progress" ref="progress">
+                                    <div className="progress-buffered" ref="buffered" ></div>
+                                    <div className="progress-played" ref="played"></div>
+                                </div>
+
+                            </div>
                             <div className="time">
                                 <div className="total-time">{this.state.totalTime}</div>
-                                <div className="remain-time">2：59</div>
+                                <span>/</span>
+                                <div className="remain-time">{this.state.remainTime}</div>
                             </div>
                         </div>
                     </div>
