2fba9334bffcb69f4fdb383c40242a0e755a34e1
zhouhaitao <capslock.zhou@tenxcloud.com>
2019-11-08 18:43:20 +0800

    更新资源 && 修复样式问题

--- src/App.js
+++ src/App.js
@@ -7,70 +7,48 @@ class App extends Component {
         super(props)
         this.state={
             songInfo:[
-                {
-                    src:"http://qqma.tingge123.com:83/123/2016/10/青蛙乐队 - 小跳蛙.mp3",
-                    artist:"青蛙乐队",
-                    name:"小跳蛙",
-                    img:"http://singerimg.kugou.com/uploadpic/softhead/400/20160913/20160913140233132.jpg",
-                    id:"66575568441"
-                },
-                {
-                    src:"http://qqma.tingge123.com:83/20081119/甜甜的.mp3",
-                    artist:"周杰伦",
-                    name:"甜甜的",
-                    img:"http://singerimg.kugou.com/uploadpic/softhead/400/20171026/20171026100450393.jpg",
-                    id:"66575568442"
-                },
-                {
-                    src:"http://qqma.tingge123.com:83/20081120/不得不爱.mp3",
-                    artist:"潘玮柏",
-                    name:"不得不爱",
-                    img:"https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=781787101,1026788150&fm=27&gp=0.jpg",
-                    id:"66575568443"
-                },
-                {
-                    src:"http://qqma.tingge123.com:83/123/2016/07/张韶涵 - 再见之前.mp3",
-                    artist:"张韶涵",
-                    name:"再见之前",
-                    img:"http://games.enet.com.cn/jzimages/201009/s1283410316.jpg",
-                    id:"66575568444"
-                },
-                {
-                    src:"http://qqma.tingge123.com:83/123/2016/05/G.E.M.邓紫棋 - 画.mp3",
-                    artist:"邓紫棋",
-                    name:"画",
-                    img:"http://img.18183.com/uploads/allimg/140510/41-140510145612.jpg",
-                    id:"66575568445"
-                },
-                {
-                    src:"http://qqma.tingge123.com:83/123/2014/12/无字碑-张靓颖.mp3",
-                    artist:"张靓颖",
-                    name:"无字碑",
-                    img:"http://singerimg.kugou.com/uploadpic/softhead/400/20170628/20170628110356447.jpg",
-                    id:"66575568446"
-                },
-                {
-                    src:"http://qqma.tingge123.com:83/123/2010/09/叶子-阿桑.mp3",
-                    artist:"阿桑",
-                    name:"叶子",
-                    img:"http://star.kuwo.cn/star/starheads/180/66/26/1776695622.jpg",
-                    id:"66575568447"
-                },
-                {
-                    src:"http://qqma.tingge123.com:83/20081117/认真的雪.mp3",
-                    artist:"薛之谦",
-                    name:"认真的雪",
-                    img:"http://img14.3lian.com/201512/02/9478d19283ce6f990741fbac92203132.jpg",
-                    id:"66575568449"
-                },
-
-                {
-                    src:"http://qqma.tingge123.com:83/20081117/杀手.mp3",
-                    artist:"林俊杰",
-                    name:"杀手",
-                    img:"http://wenwen.soso.com/p/20070621/20070621060503-1458706809.jpg",
-                    id:"66575568448"
-                },
+              {
+                src:"http://neroht.com/%E6%AD%A2%E6%88%98%E4%B9%8B%E6%AE%87-piano.mp3",
+                artist:"张斗完",
+                name:"止战之殇（纯钢琴）",
+                img:"http://neroht.com/jay.jpg",
+                id:"66575568441"
+              },
+              {
+                src:"http://neroht.com/%E7%91%BE%E5%A7%9DHikari%20-%20%E5%A4%A7%E6%B0%BF%E6%AD%8C%EF%BC%88%E6%88%8F%E8%85%94%E7%89%88%EF%BC%89%EF%BC%88Cover%EF%BC%9Ailem%EF%BC%89.mp3",
+                artist:"瑾姝Hikari",
+                name:"大氿歌",
+                img:"http://neroht.com/daguige.jpg",
+                id:"66575568442"
+              },
+              {
+                src:"http://neroht.com/%E7%91%BE%E5%A7%9DHikari%20-%20%E8%B5%A4%E4%BC%B6%EF%BC%88Cover%EF%BC%9AHITA%EF%BC%89.mp3",
+                artist:"瑾姝Hikari",
+                name:"赤伶",
+                img:"http://neroht.com/chiling.jpg",
+                id:"66575568443"
+              },
+              {
+                src:"http://neroht.com/%E4%BC%A6%E6%A1%91%20-%20%E7%83%9F%E9%9B%A8%E8%A1%8C%E8%88%9F.mp3",
+                artist:"伦桑",
+                name:"烟雨行舟",
+                img:"http://neroht.com/yanyuxingzhou.jpg",
+                id:"66575568444"
+              },
+              {
+                src:"http://neroht.com/%E8%A2%81%E9%9B%A8%E6%A1%90%20-%20%E6%8A%98%E6%9C%88%E6%BB%A1%E5%85%9C.mp3",
+                artist:"袁雨桐",
+                name:"折月满兜",
+                img:"http://neroht.com/zheyuemandou.jpg",
+                id:"66575568445"
+              },
+              {
+                src:"http://neroht.com/Take%20Your%20Time%20%20-%20alexandr.mp3",
+                artist:"Alexandr",
+                name:"Take Your Time",
+                img:"http://neroht.com/alexander.png",
+                id:"66575568446"
+              },
             ]
         };
         this.delSong = this.delSong.bind(this)
