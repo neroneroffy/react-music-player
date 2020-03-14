20a00cfdda8f4031ef399b820140ea2bc71a9f43
neroneroffy <haitaocapslock@outlook.com>
2020-03-14 18:38:04 +0800

    fix: types file's layout

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,81 +1,81 @@
 type PlayMode = 'order' | 'random' | 'loop'
 declare namespace coolPlayerTypes {
-    interface IPlayerProps {
-        autoPlay?: boolean
-        playing?: boolean
-        showPlayDetail?: boolean
-        onDelete?: (index: number, id: string) => void
-        volume?: number
-        onVolumeChange?: (volume: number) => void
-        data: IAudio[]
-        playListPlaceholder?: string
-        currentAudio?: IAudio
-        zIndex?: number
-        onLyricMatched?: (lyric: ILyric[], currentIndex: number) => void
-        showLyricNormal?: boolean
-        showLyricMini?: boolean
-        onMusicChange?: (id: string, currentMusic: IAudio) => void
-        onPlayStatusChange?: (currentMusic: IAudio, isPlaying: boolean) => void
-        onModeChange?: (currentMode: PlayMode, prevMode: PlayMode) => void
-        playMode?: PlayMode
-        lyric?: string
-        tLyric?: string
-        lyricLoading?: boolean
-        lyricPlaceholder?: React.ReactNode | string
-        avatarPlaceholder?: React.ReactNode
-        actions?: Array<(data: IAudio) => React.ReactNode>
-        musicActions?: Array<(data: IAudio, active?: boolean) => React.ReactNode>
-        playListHeader? : {
-            headerLeft?: React.ReactNode | string,
-            headerRight?: React.ReactNode | string,
-        }
+  interface IPlayerProps {
+    autoPlay?: boolean
+    playing?: boolean
+    showPlayDetail?: boolean
+    onDelete?: (index: number, id: string) => void
+    volume?: number
+    onVolumeChange?: (volume: number) => void
+    data: IAudio[]
+    playListPlaceholder?: string
+    currentAudio?: IAudio
+    zIndex?: number
+    onLyricMatched?: (lyric: ILyric[], currentIndex: number) => void
+    showLyricNormal?: boolean
+    showLyricMini?: boolean
+    onMusicChange?: (id: string, currentMusic: IAudio) => void
+    onPlayStatusChange?: (currentMusic: IAudio, isPlaying: boolean) => void
+    onModeChange?: (currentMode: PlayMode, prevMode: PlayMode) => void
+    playMode?: PlayMode
+    lyric?: string
+    tLyric?: string
+    lyricLoading?: boolean
+    lyricPlaceholder?: React.ReactNode | string
+    avatarPlaceholder?: React.ReactNode
+    actions?: Array<(data: IAudio) => React.ReactNode>
+    musicActions?: Array<(data: IAudio, active?: boolean) => React.ReactNode>
+    playListHeader? : {
+      headerLeft?: React.ReactNode | string,
+      headerRight?: React.ReactNode | string,
     }
-    interface IAudio {
-        src: string
-        artist: string
-        name: string
-        img: string
-        id: string
-        lyric?: string
-        tLyric?: string
+  }
+  interface IAudio {
+    src: string
+    artist: string
+    name: string
+    img: string
+    id: string
+    lyric?: string
+    tLyric?: string
+  }
+  interface ILyric {
+    time: number | string
+    lyric: string
+  }
+  interface ITLyric {
+    [ key: number ]: string
+    [ key: string ]: string
+  }
+  namespace lyricDetail {
+    interface ILyricDetailProps {
+      lyric: ILyric[]
+      tLyric?: ITLyric
+      lyricIndex: number
+      info: IAudio
+      loading: boolean
+      lyricFullScreen: boolean
+      lyricPlaceholder: React.ReactElement | React.ReactNode | string
+      onSetProgressWithScroll: (time: number) => void
     }
-    interface ILyric {
-        time: number | string
-        lyric: string
+  }
+  namespace lyricMini {
+    interface ILyricMiniProps {
+      lyric: ILyric[]
+      tLyric?: ITLyric
+      lyricIndex: number
     }
-    interface ITLyric {
-      [ key: number ]: string
-      [ key: string ]: string
+  }
+  namespace lyricNormal {
+    interface ILyricNormalProps {
+      lyric: ILyric[]
+      tLyric?: ITLyric
+      lyricIndex: number
+      info: IAudio
+      loading: boolean
+      lyricPlaceholder: React.ReactElement | React.ReactNode | string
     }
-    namespace lyricDetail {
-        interface ILyricDetailProps {
-            lyric: ILyric[]
-            tLyric?: ITLyric
-            lyricIndex: number
-            info: IAudio
-            loading: boolean
-            lyricFullScreen: boolean
-            lyricPlaceholder: React.ReactElement | React.ReactNode | string
-            onSetProgressWithScroll: (time: number) => void
-        }
-    }
-    namespace lyricMini {
-        interface ILyricMiniProps {
-            lyric: ILyric[]
-            tLyric?: ITLyric
-            lyricIndex: number
-        }
-    }
-    namespace lyricNormal {
-        interface ILyricNormalProps {
-            lyric: ILyric[]
-            tLyric?: ITLyric
-            lyricIndex: number
-            info: IAudio
-            loading: boolean
-            lyricPlaceholder: React.ReactElement | React.ReactNode | string
-        }
-    }
+  }
 }
 
 export { coolPlayerTypes }
